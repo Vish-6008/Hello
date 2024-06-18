70a69b01fdad1803451aad3acadc5cc4a09a94c3
DELL <DELL@DESKTOP-8S0EJGG>
2024-06-18 11:23:11 +0530

    Commit

---
+++ helloword/src/helloword/HelloWordDemo.java
@@ -0,0 +1,11 @@
+package helloword;
+
+public class HelloWordDemo {
+
+	public static void main(String[] args) {
+		System.out.println("This is demo for this project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
