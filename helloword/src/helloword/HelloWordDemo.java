4c8f8a6e0d60188daad85a0878ccc9d2d07ca63a
Vish-6008 <vishwanath.d@crevavi.com>
2024-06-24 17:20:43 +0530

    Update HelloWordDemo.java

--- helloword/src/helloword/HelloWordDemo.java
+++ helloword/src/helloword/HelloWordDemo.java
@@ -1,3 +1,19 @@
+/********************************************************************************************
+ *   COPYRIGHT (C) 2024 CREVAVI TECHNOLOGIES PVT LTD
+ *   The reproduction, transmission or use of this document/file or its
+ *   contents is not permitted without written authorization.
+ *   Offenders will be liable for damages. All rights reserved.
+ *---------------------------------------------------------------------------
+ *   Purpose:  Below java code print's the message.
+ *   Project: Hello-World 
+ *   Platform: Cross-platform (Windows, macOS, Linux)
+ *   Compiler: JDK-22
+ *   IDE:      Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
+ *	 Version: 2024-03 (4.31.0)
+ *   Build id: 20240307-1437
+ * ***********************************************************/
+
+
 package helloword;
 
 public class HelloWordDemo {
